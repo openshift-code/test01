2387191b8d4e6e966212dfc4ee165e84621d7309
test <test@test.com>
2020-04-05 18:59:18 +0200

    [001] Init commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/com/example/demo/Example.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+
+
+@RestController
+public class Example {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @PostConstruct
+    public static void init() {
+        System.out.println("Init started.");
+
+    }
+
+    @GetMapping("/tst")
+    public String tst() {
+        System.out.println("GET mapping");
+        return LocalDateTime.now().toString();
+    }
+}
