244eec24dbf7e0f904d499353fb526d63d1c4231
test <test@test.com>
2020-04-05 19:32:01 +0200

    [002] JAR to WAR

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,7 +10,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication
+		extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
